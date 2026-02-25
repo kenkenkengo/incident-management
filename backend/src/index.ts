755a960beb9ebaee35564c4b6524f11337d0c49f
Yohei Suda <yohei.suda@generosity.co.jp>
2026-02-25 15:39:23 +0900

    wip

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,15 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/aws-lambda'
 
+
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/', async (c) => {
+  return c.text('Hello, World!')
+})
+
+app.get('/signin', async (c) => {
+
 })
 
 export const handler = handle(app)
--- backend/sst.config.ts
+++ backend/sst.config.ts
@@ -1,13 +1,32 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
+const createSite = () => {
+  return new sst.aws.StaticSite("Site", {
+    path: "../frontend",
+    build: {
+      command: "npm run build",
+      output: "dist",
+    },
+  });
+};
+
+const userPool = new sst.aws.CognitoUserPool("UserPool")
+
 export default $config({
   app(input) {
     return {
-      name: "backend",
+      name: "generosity-incident-management",
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
     };
   },
-  async run() {},
+
+  async run() {
+    createSite();
+    new sst.aws.Function("Hono", {
+      url: true,
+      handler: "src/index.handler",
+    });
+  },
 });
